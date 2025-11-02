054d9b0cbf8c676a283de6fb4bb71cd2be81893c
Skorgum <skorgum@gmail.com>
2025-11-01 23:08:10 -0400

    split into 2 scripts

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+from stats import get_book_text
+
+def main():
+      filepath = "books/frankenstein.txt"
+      num_words = len(get_book_text(filepath).split())
+      print(f"Found {num_words} total words")
+main()
+      
