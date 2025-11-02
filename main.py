e6ffdd47f2406b73d0983d91e2f5d020b0fdcaad
Skorgum <skorgum@gmail.com>
2025-11-02 00:52:55 -0400

    finished

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from stats import get_book_text
+import sys
+from stats import get_book_text, count_characters, sort_count
 
 def main():
-      filepath = "books/frankenstein.txt"
-      num_words = len(get_book_text(filepath).split())
-      print(f"Found {num_words} total words")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+    text = get_book_text(filepath)
+    num_words = len(text.split())
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    characters = sort_count(count_characters(text))
+    for character in characters:
+        ch = character["char"]
+        if ch.isalpha():
+            print(f"{ch}: {character['num']}")
+    print("============= END ===============")
+    
 main()
-      
--- stats.py
+++ stats.py
@@ -1,3 +1,20 @@
 def get_book_text(filepath):
         with open(filepath) as f:
             return f.read()
+def count_characters(text):
+        counts = {}
+        for i in text.lower():
+            if i in counts:
+                counts[i] += 1
+            else:
+                counts[i] = 1
+        
+        return counts
+def sort_on(item):
+     return item["num"]
+def sort_count(counts):
+     sortedcounts = []
+     for ch, num in counts.items():
+        sortedcounts.append({"char": ch, "num": num})
+     sortedcounts.sort(reverse=True, key=sort_on)
+     return sortedcounts
